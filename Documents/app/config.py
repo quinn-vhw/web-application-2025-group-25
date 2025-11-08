dda4921e519642a88a088e1525dcac123af7baf6
quinn-vhw <quinn.vhw@gmail.com>
2025-11-08 16:09:52 +0100

    test

--- Documents/app/config.py
+++ Documents/app/config.py
@@ -2,3 +2,7 @@ class Config:
     SECRET_KEY = 'Groep25&groep25'
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres: Groep25&groep25@db.hsjjntcivrvlguqfdqxl.supabase.co:5432/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+
+#test
