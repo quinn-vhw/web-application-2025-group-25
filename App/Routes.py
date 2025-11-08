30f93432157ab67e03c08c665f23839e1af32dc2
irene857 <irene.vereecken@ugent.be>
2025-11-08 14:41:03 +0100

    Create Routes.py

---
+++ App/Routes.py
@@ -0,0 +1,40 @@
+from flask import Blueprint, request, redirect, url_for, render_template, session
+from .models import db, User
+
+main = Blueprint('main', __name__)
+
+@main.route('/')
+def index():
+    if 'user_id' in session:
+        user = User.query.get(session['user_id'])
+        return f'Logged in as {user.email}'
+    return 'You are not logged in <a href="/login">login</a> <a href="/register">register</a>'
+
+@main.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        email = request.form['email']
+        if User.query.filter_by(email=email).first() is None:
+            new_user = User(email=email)
+            db.session.add(new_user)
+            db.session.commit()
+            session['user_id'] = new_user.id
+            return redirect(url_for('main.index'))
+        return 'Email already registered'
+    return render_template('register.html')
+
+@main.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        user = User.query.filter_by(email=email).first()
+        if user:
+            session['user_id'] = user.id
+            return redirect(url_for('main.index'))
+        return 'User not found'
+    return render_template('login.html')
+
+@main.route('/logout')
+def logout():
+    session.pop('user_id', None)
+    return redirect(url_for('main.index'))
